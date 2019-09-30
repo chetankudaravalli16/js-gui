301e421fff57d3987973b99f14bd73d29f9e9a87
chetan <chetankudaravalli16@gmail.com>
2019-09-30 13:15:28 -0500

    Modified the app into area calculator

--- main.js
+++ main.js
@@ -4,17 +4,15 @@ const updateWithAdd = async (event) =>
   {
     
     const s = parseInt(document.querySelector('#guest').value)
-    const i = parseInt(document.querySelector('#firstNumber').value)
-    const j = parseInt(document.querySelector('#secondNumber').value)
-    const result =  (s)*(i)*(j)
-    document.querySelector('#result').innerHTML ="volume is : " +result
+   
+    
+    const result =  Math.PI*(s)*(s)
+    document.querySelector('#result').innerHTML ="Area is : " +result
 
   }
 
 
 
-// delegate to dynamic elements (e.g. when testing)
-// focusout is like blur, but it bubbles up
 
 
 
